3cc0c3e80b14f9086e829b96888f2a847c1b6d00
IT21375132 <gokulabisheak12@gmail.com>
2023-09-01 19:48:44 +0530

    Moved side navigation links to dedicated JSON file

--- client/admin/src/components/SideNav/index.jsx
+++ client/admin/src/components/SideNav/index.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faCartShopping, faChartSimple, faUsers } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import sideNavigation from '../../data/sideNavigation.json'
 
 const SideNav = () => {
 
@@ -18,29 +19,9 @@ const SideNav = () => {
         console.log(isOpen)
     }
 
-    const adminNavigation = [
-        {
-            name: "Dashboard",
-            icon: faChartSimple,
-            link: "/"
-        },
-
-        {
-            name: "User Management",
-            icon: faUsers,
-            link: "/users"
-        },
-
-        {
-            name: 'Product Management',
-            icon: faCartShopping,
-            link: '/products'
-        }
-    ]
-
     return (
         <>
-            <div className={`h-screen bg-gradient-to-t from-cyan-300 to-cyan-700 w-fit ${isOpen ? 'absolute md:fixed' : ''}`}>
+            <div className={`h-screen bg-gradient-to-t from-cyan-300 to-cyan-700 w-fit z-50 ${isOpen ? 'absolute md:fixed' : ''}`}>
                 <div className='px-[20px] h-[64px] uppercase text-white font-bold text-xl w-full flex justify-center items-center gap-[20px]'>
                     <div className={`md:flex text-center ${isOpen ? 'block' : 'hidden'}`}>
                         Admin Panel
@@ -52,7 +33,7 @@ const SideNav = () => {
                     </div>
                 </div>
                 <div className='grid'>
-                    {adminNavigation && adminNavigation.length ? adminNavigation.map((item) => (
+                    {sideNavigation && sideNavigation.data && sideNavigation.data.length ? sideNavigation.data.map((item) => (
                         <Link key={item.link} to={item.link}>
                             <div className={`px-[20px] py-[10px] w-full flex items-center gap-[10px] hover:bg-black hover:bg-opacity-20 ${item.link == activePage ? 'bg-black bg-opacity-40' : ''}`}>
                                 <div className='text-white w-[24px]'>
