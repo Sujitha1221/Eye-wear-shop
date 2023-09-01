7fe4ce20b131f432a169d06daf3a8b882dc409c5
IT21375132 <gokulabisheak12@gmail.com>
2023-09-01 19:49:11 +0530

    Minor UI fixes

--- client/admin/src/components/Header/index.jsx
+++ client/admin/src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <>
-            <div className='h-[64px] w-full flex justify-between px-[20px] shadow-[0px_0px_10px_0px_rgba(0,0,0,0.5)]'>
+            <div className='h-[64px] w-full flex justify-between px-[20px] shadow-[0px_0px_10px_0px_rgba(0,0,0,0.5)] z-10 bg-white'>
 
             </div>
         </>
