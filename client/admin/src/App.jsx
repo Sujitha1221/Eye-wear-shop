8a79aff4bca5312ada3a0d649f3b0f6787dabc56
IT21375132 <gokulabisheak12@gmail.com>
2023-09-10 12:00:23 +0530

    Bug Fix

--- client/admin/src/App.jsx
+++ client/admin/src/App.jsx
@@ -1,8 +1,6 @@
 import FrontendRoutes from "./routes";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import { library } from "@fortawesome/fontawesome-svg-core";
 
 function App() {
 
